e77734424c2e9dd6583f51d707c5065761ccbaf7
Benjamin Cheng <bencheng200@gmail.com>
2020-12-11 23:59:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,8 +73,24 @@ class Student
      DB[:conn].execute(sql).collect do |row|
        self.new_from_db(row)
     end
+  end
 
- end
+  def self.all_students_in_grade_10
+    sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE students.grade = 10
+     SQL
+     DB[:conn].execute(sql).collect do |row|
+       self.new_from_db(row)
+    end
+  end
 
 
+
+
+
+
+
+  
 end
