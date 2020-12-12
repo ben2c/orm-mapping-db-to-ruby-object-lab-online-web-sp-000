5ea10080fdb55fe119937f01b071d2b4eb0b88a5
Benjamin Cheng <bencheng200@gmail.com>
2020-12-12 00:02:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,7 +86,7 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10 (x)
     sql = <<-SQL
      SELECT *
      FROM students
@@ -108,7 +108,16 @@ class Student
     end.first
   end
 
-
+  def self.all_students_in_grade_X (x)
+    sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE students.grade = x
+     SQL
+     DB[:conn].execute(sql).collect do |row|
+       self.new_from_db(row)
+    end.first
+  end
 
 
 
