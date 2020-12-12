c2e32465bd153fa2e99e4919077abe61559764df
Benjamin Cheng <bencheng200@gmail.com>
2020-12-12 00:03:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -112,7 +112,7 @@ class Student
     sql = <<-SQL
      SELECT *
      FROM students
-     WHERE students.grade = x
+     WHERE students.grade = ?
      SQL
      DB[:conn].execute(sql).collect do |row|
        self.new_from_db(row)
