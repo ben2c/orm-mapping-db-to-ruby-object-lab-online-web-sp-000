e3302b671c23a0ecfd1252ae2050b7d7d0f254fa
Benjamin Cheng <bencheng200@gmail.com>
2020-12-12 00:05:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -115,7 +115,7 @@ class Student
      FROM students
      WHERE students.grade = ?
      SQL
-     DB[:conn].execute(sql).collect do |row|
+     DB[:conn].execute(sql, x).collect do |row|
        self.new_from_db(row)
     end
   end
